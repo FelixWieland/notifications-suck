49ca3edb8a9447ab6e6e6e54ceafa3087d2ad01e
Felix Wieland <felix.wieland@mail.de>
2022-07-07 17:53:22 +0200

    base functionality

--- main.go
+++ main.go
@@ -1,17 +1,94 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"os"
+	"strconv"
+	"time"
 )
+
+type Notification struct {
+	Time    time.Time `json:"time"`
+	Message string    `json:"message"`
+}
+
+var notifications = []Notification{}
 
 func main() {
 	port := os.Getenv("PORT")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World"))
 	})
+
+	http.HandleFunc("/create", func(w http.ResponseWriter, r *http.Request) {
+		keys, ok := r.URL.Query()["message"]
+		if !ok || len(keys[0]) < 1 {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("message is missing"))
+			return
+		}
+		message := keys[0]
+
+		n := createNotification(message)
+		b, err := json.Marshal(n)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		w.Write(b)
+	})
+
+	http.HandleFunc("/from", func(w http.ResponseWriter, r *http.Request) {
+		keys, ok := r.URL.Query()["from"]
+		if !ok || len(keys[0]) < 1 {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("from is missing"))
+			return
+		}
+
+		fromRaw := keys[0]
+		from, _ := strconv.Atoi(fromRaw)
+
+		ns := []Notification{}
+		for _, n := range notifications {
+			if n.Time.Unix() > int64(from) {
+				ns = append(ns, n)
+			}
+		}
+
+		b, err := json.Marshal(ns)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		w.Write(b)
+	})
+
+	http.HandleFunc("/all", func(w http.ResponseWriter, r *http.Request) {
+		b, err := json.Marshal(notifications)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		w.Write(b)
+	})
+
 	if len(port) == 0 {
 		port = "80"
 	}
 	http.ListenAndServe(":"+port, http.DefaultServeMux)
+}
+
+func createNotification(message string) Notification {
+	n := Notification{
+		Time:    time.Now(),
+		Message: message,
+	}
+	notifications = append(notifications, n)
+	return n
 }
